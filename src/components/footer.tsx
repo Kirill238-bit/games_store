403107a66f8b7c74f2b773c0c9d323e62a675853
kirill <kirill@yandery>
2023-06-27 02:44:58 +0300

    Feat(footer,navbar)first sketch

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,50 @@
+import style from "../styles/footer.module.scss"
+import Image from "next/image"
+
 export default function Footer(){
     return(
-        <div>подвал</div>
+        <div className={style.wrapper}>
+            <div className={style.conteiner}>
+                <div className={style.info}>
+                    <div className={style.info_icon}>
+                        <div className={style.info_icon_img}>
+                            <Image 
+                               src="/images/control_icon.png"
+                               alt="контроллер"
+                               height={50}
+                               width={50}/>
+                        </div>
+                        <div className={style.info_icon_title}>
+                            <div>Games store</div>
+                        </div>
+                    </div>
+                    <div className={style.info_menu}>
+                        <div>о компании</div>
+                        <div>контакты</div>
+                        <div>вакансии</div>
+                        <div>поддержка</div>
+                        <div>подарочные карты</div>
+                        <div>соглашение</div>
+                        <div>конфиденциальность</div>
+                        <div>возвраты</div>
+                        <div>правовая информация</div>
+                    </div>
+                    <div className={style.info_social}>
+                        <div> иконка вк</div>
+                        <div> иконка телеги</div>
+                        <div> иконка дс</div>
+                        <div> иконка ютуба</div>
+                    </div>
+                </div>
+
+                <div className={style.images}>
+                        <div>иконка визы</div>
+                        <div>иконка мастеркард</div>
+                        <div>иконка мир</div>
+                        <div>иконка киви</div>
+                        <div>иконка кассы</div>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,36 @@
+import style from "../styles/navbar.module.scss"
+import Image from "next/image"
+
 export default function Navbar(){
     return(
-        <div>навигация по сайту</div>
+        <div className={style.wrapper}>
+            <div className={style.icon_conteiner}>
+                <div className={style.icon_conteiner_image}>
+                    <Image 
+                      src="/images/control_icon.png"
+                      alt="контроллер"
+                      height={50}
+                      width={50}/>
+                </div>
+                <div className={style.icon_conteiner_title}>
+                    <div>Games store</div>
+                </div>
+            </div>
+
+            <div className={style.games_conteiner}>
+                <div><button>каталог игр </button></div>
+            </div>
+
+            <div className={style.basket_conteiner}>
+                <button>
+                    <Image
+                       src="/images/basket_icon.png"
+                       alt="корзина"
+                       height={50}
+                       width={50}
+                       />
+                </button>
+            </div>
+        </div>
     )
 }
